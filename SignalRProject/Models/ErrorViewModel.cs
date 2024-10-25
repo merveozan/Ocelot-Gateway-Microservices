009d3f9d6aacac2f2f4e3bdcd03bf232b40d61b9
merveozan <129623198+merveozan@users.noreply.github.com>
2024-10-25 16:09:47 +0300

    Delete SignalRProject directory

--- SignalRProject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SignalRProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
