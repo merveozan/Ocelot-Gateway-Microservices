4450b590003ad8f78c4fa281f0a3dd48cebd44be
Merve Nur Ozan <merve.ozan@trive.com>
2024-12-15 21:54:19 +0300

    Removed all

--- IdentityServerApi/Models/AuthenticationRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace IdentityServerApi.Models
-{
-    public class AuthenticationRequest
-    {
-        public string UserName { get; set; }
-
-        public string Password { get; set; }
-    }
-}
